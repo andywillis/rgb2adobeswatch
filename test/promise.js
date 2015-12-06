e7219bcde87d17acb7b331ec146273b3071ba9cc
Andy Willis <github@awillis.fastmail.com>
2015-12-06 06:43:59 +0000

    Added promise test

---
+++ test/promise.js
@@ -0,0 +1,8 @@
+var test = require('tape');
+var promise = require('../dist/lib/promise').default;
+
+test('promise is a function', function (t) {
+  t.plan(1);
+  t.equal('function', typeof promise, 'promise is a function');
+  t.end();
+});
